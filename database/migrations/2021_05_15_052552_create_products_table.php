ff8066c7e3048bbc6885a7c82d6f9e0d08e3b9fb
roshan <stha901@gmail.com>
2021-05-15 13:16:12 +0545

    Migration created for products and reviews

--- database/migrations/2021_05_15_052552_create_products_table.php
+++ database/migrations/2021_05_15_052552_create_products_table.php
@@ -16,6 +16,11 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('detail');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
         });
     }
 
--- database/migrations/2021_05_15_052639_create_reviews_table.php
+++ database/migrations/2021_05_15_052639_create_reviews_table.php
@@ -15,7 +15,12 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
         });
     }
 

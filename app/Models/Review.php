c3194a7bd8e340e63213cc3117b39686e00d708a
roshan <stha901@gmail.com>
2021-05-18 12:55:13 +0545

    Products resource and relationship created

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
